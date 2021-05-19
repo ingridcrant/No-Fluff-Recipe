3d880d8efdd958b5004d61fdc62fca85cb565ef1
ingridcrant <ingridcrant@gmail.com>
2021-05-19 11:20:59 -0400

    got prices kinda working

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 # scraping student housing
@@ -6,5 +7,8 @@ page = urlopen(url)
 
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
 
-print(html)
+prices = soup.find_all("td", {"class": "t-last"})
+
+print(prices)
