828f0ae237d827922bdb9a48bc02b1f3269e9921
ingridcrant <ingridcrant@gmail.com>
2021-06-02 18:22:25 -0400

    made css and made improvements to generated website

--- api/api.py
+++ api/api.py
@@ -7,14 +7,23 @@ app = Flask(__name__)
 def get_post_javascript_data():
     html = request.form['html']
     html_soup = BeautifulSoup(html, 'html.parser')
+    title = html_soup.find('div', class_='intro article-info')
     image = html_soup.find('div', class_='inner-container js-inner-container image-overlay')
-    ingredients = html_soup.find('div', class_='recipe-shopper-wrapper')
-    instructions = html_soup.find('fieldset', class_='instructions-section__fieldset')
+    originalservingamt = html_soup.find('div', class_='recipe-adjust-servings__original-serving')
+    ingredients = html_soup.find('ul', class_='ingredients-section')
+    instructions = html_soup.find('ul', class_='instructions-section')
 
-    content = str(image.img) + str(ingredients) + str(instructions)
+    originalserving = '<h3>'+originalservingamt.text+'</h3>'
+    ingredientsheader = '<h2>Ingredients</h2>'
+    instructionsheader = '<h2>Instructions</h2>'
+    header = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><link rel="stylesheet" href="recipestyle.css" /><title>Recipe</title></head>'
+    
+    content = header + str(title) + str(image.img) + originalserving + ingredientsheader + str(ingredients) + instructionsheader + str(instructions)
 
     with open('recipe.html','w') as f:
+        f.write('<div class="container">')
         f.write(content)
+        f.write('</div>')
     
     f.close()
 
