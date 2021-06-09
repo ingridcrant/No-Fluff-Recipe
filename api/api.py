2a717bc3179aa6d248ca67ca12e9f05033652fcd
ingridcrant <ingridcrant@gmail.com>
2021-06-09 18:47:43 -0400

    backend works with more templates

--- api/api.py
+++ api/api.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from flask import Flask, request, jsonify
+import re
 
 app = Flask(__name__)
 
@@ -11,6 +12,25 @@ def get_post_javascript_data():
     content = '<div class="container">'
     for EachPart in html_soup.select('div[class*="wprm-recipe-container"]'):
         content += str(EachPart)
+    
+    if html_soup.find('div', id=re.compile('^wpurp-container-recipe-')):
+        content += str(html_soup.find('div', id=re.compile('^wpurp-container-recipe-')))
+    
+    if html_soup.find('div', id=re.compile('^tasty-recipes')):
+        content += str(html_soup.find('div', id=re.compile('^tasty-recipes')))
+    
+    if html_soup.find('div', id=re.compile('^post-recipe')):
+        content += str(html_soup.find('div', id=re.compile('^post-recipe')))
+    
+    if html_soup.find('div', id=re.compile('^structured-project-content')):
+        content += str(html_soup.find('div', id=re.compile('^structured-project-content')))
+
+    if html_soup.find('div', itemtype="http://schema.org/Recipe"):
+        content += str(html_soup.find('div', itemtype="http://schema.org/Recipe"))
+    
+    if html_soup.find('div', itemtype="https://schema.org/Recipe"):
+        content += str(html_soup.find('div', itemtype="https://schema.org/Recipe"))
+    
     content += "</div>"
     
     response = jsonify(recipe=content)
